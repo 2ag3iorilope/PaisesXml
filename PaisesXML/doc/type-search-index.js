fbd02f94812eea4a2f29d266bb522c7cfec25180
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-11-14 09:49:04 +0100

    Last

---
+++ PaisesXML/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"PaisesXML","l":"DirektorioaUtils"},{"p":"PaisesXML","l":"FitxategiOperazioak"},{"p":"PaisesXML","l":"Main"}];updateSearchResults();
